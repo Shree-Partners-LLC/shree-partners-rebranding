9c68b23fe1fee62dc50b87752fa0b600eca3d9ad
Riya <riyaaneja336@gmail.com>
2026-07-03 12:20:22 +0530

    dummy pages added

---
+++ src/components/Privacy.jsx
@@ -0,0 +1,9 @@
+const Privacy = () => {
+  return (
+    <div>
+      <h1 style={{ color: "black" }}>Privacy Policy</h1>
+    </div>
+  );
+};
+
+export default Privacy;
---
+++ src/components/Terms.jsx
@@ -0,0 +1,9 @@
+const Terms = () => {
+  return (
+    <div>
+      <h1 style={{ color: "black" }}>Terms & Conditions</h1>
+    </div>
+  );
+};
+
+export default Terms;
