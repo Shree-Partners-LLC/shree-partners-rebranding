028ad9fef0eebb084e835d40425e5b2d8c310988
sanyaverma27 <sanyaverma12871@gmail.com>
2026-07-16 13:37:05 +0530

    ui improved added hover effect'

--- src/components/Travel.jsx
+++ src/components/Travel.jsx
@@ -1,9 +1,27 @@
-const Travel = () => {
+import "./Travel.css";
+import travelImg from "../assets/travel-and-hospitality.jpg";
+function Travel() {
   return (
-    <div>
-      <h1 style={{ color: "black" }}>Travel</h1>
-    </div>
-  );
-};
+    <>
+      <section className="travel-hero">
+        <div className="travel-container">
+          <div className="travel-left">
+            <h1>Travel And Hospitality</h1>
 
+            <p>
+              Transform your business with
+industry-leading technology
+            </p>
+          </div>
+
+          <div className="travel-right">
+            <img src={travelImg} alt="Travel Shree Partners" />
+          </div>
+        </div>
+      </section>
+    
+     
+    </>
+  );
+}
 export default Travel;
